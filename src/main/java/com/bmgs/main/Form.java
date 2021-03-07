e2ecd1288be80724c3eacd30628febd3a323a200
Britney <bakebr75@gmail.com>
2021-03-07 13:58:55 -0500

    fixed merge conflict

--- src/main/java/com/bmgs/main/Form.java
+++ src/main/java/com/bmgs/main/Form.java
@@ -13,7 +13,6 @@ public class Form {
 
     String title;
 
-    //TODO Replace String to Question Type
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<Question> questionList;
 
@@ -87,7 +86,7 @@ public class Form {
     }
 
     /**
-     * Get question list.
+     * Get question list
      * @return
      */
     public List<Question> getQuestionList(){
